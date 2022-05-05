3e2be7fe6736e2717ab1b6f1dde5c9aa4d59d77f
filipjovicccc <filipjovicccc@gmail.com>
2022-05-05 12:56:20 +0200

    update

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
+import { useState } from "react";
+import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className="container">
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear List</button>
+      </article>
+    </main>
   );
 }
 
